636b92ff9783a167c389d97ccda305b20975fcde
Raphael Kathambana <maya12raph@gmail.com>
2023-11-25 17:00:04 +0300

    added default running page

--- GUIs/quizzify/unused/Quizzify.java
+++ GUIs/quizzify/unused/Quizzify.java
@@ -1,8 +1,12 @@
+package quizzify.unused;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package quizzify.unused;
+
+
+import javax.swing.SwingUtilities;
+import quizzify.EntryPage;
 
 /**
  *
@@ -14,7 +18,7 @@ public class Quizzify {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingUtilities.invokeLater(() -> new EntryPage().setVisible(true));
     }
     
 }
